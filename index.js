2e59185261d36a0a28d003ddf35b8be49763266b
huangxinyueus <164641405+huangxinyueus@users.noreply.github.com>
2026-08-24 08:53:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezha.keai.pp.ua:443';        
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '78dvbmjB34Fe5xhlxJP8iIGjkgLUnOvH';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '187railway.keaiduo.kdns.fr';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYjA2ZWRhZjA4NTVkMWQ1NTE3MzUwMTRjYzI5YTA3YjgiLCJ0IjoiZWUwODFkODAtYzJiNC00YTI4LTlkODktNjg2NTg3ZTJjZmQyIiwicyI6Ik5tVmhaRGt4WlRRdFl6RmlOUzAwTm1Ka0xXSTRPRFl0WlRoaE1XWmlOelZoTWpNdyJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYjA2ZWRhZjA4NTVkMWQ1NTE3MzUwMTRjYzI5YTA3YjgiLCJ0IjoiZTY1ODZkNzctYjVjNi00ZWRlLTlmMjctNzYxNDhmOGFkYzRjIiwicyI6Ik1Ua3lOR00yT1dRdFltUXpZUzAwT1dRMkxXSXhOamt0TmpjeFpqZGhaVFkyWW1RMyJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'youxuan.yuyu2012.nyc.mn';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || 'railway187-us';                        // 节点名称
+const NAME = process.env.NAME || '187railway-us';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
